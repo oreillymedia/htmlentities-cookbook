2f2a2f1f7417798b8d2b04071d906a21d6066502
Rune Madsen <rune@runemadsen.com>
2014-05-14 15:20:12 -0400

    bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "sanders@oreilly.com"
 license           "Apache 2.0"
 description       "Installs catalogs of XHTML entities in /etc/xml for use in parsing entities"
 
-version           "0.0.1"
+version           "0.0.2"
 recipe            "default", "Installs XHTML entity catalogs"
 
 %w{ ubuntu debian }.each do |os|
